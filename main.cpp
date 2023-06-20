090d9021007d58daa3c46c586b1afc5c37bdbf64
Chamber6821 <vania.osinin@yandex.ru>
2023-06-20 12:25:34 +0300

    feat: add main.cpp and build settings

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <format>
+#include <iostream>
+
+int main() {
+  std::setlocale(LC_ALL, "Russian");
+
+  int cows = 33;
+  std::wcout << std::format(
+      L"{0} коровы, {0} коровы, {0} коровы - свежая строка\n", cows);
+}
